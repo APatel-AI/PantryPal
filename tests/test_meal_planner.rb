6f585ac14cc2c51a31d38dacace862f034328457
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-02-15 18:36:42 +0000

    add: test tamplate

---
+++ tests/test_meal_planner.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require '../meal_planner'
+
+
+class TestMealPlanner < Minitest::Test
+  def setup
+    @meal_planner = MealPlanner.new
+
+  end
+
+
+  #ADD MEAL TEST
+  def test_add_meal
+  end
+
+
+  #DISPLAY MEALS TEST
+  def test_list_meals
+  end
+
+
+  #CATEGORY SEARCH TEST
+  def test_category_search
+  end
+
+  #INGREDIENTS SEARCH TEST
+  def test_ingredients_search
+  end
+  
+end
